0a4f66720fb411b86a63e2fb2f93e3bec365ad89
awesumscott <scott@scottlowe.us>
2024-05-16 22:40:58 -0400

    explicit AL

--- AnimUtil.cs
+++ AnimUtil.cs
@@ -4,216 +4,216 @@ using NESSharp.Lib.Animation.Iterators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using static NESSharp.Core.AL;
+using static NESSharp.Core.CPU6502;
 
-namespace NESSharp.Lib.Animation {
-	public class AnimUtil : Module {
-		private Ptr _ptr;
-		private AnimationData _animData;
-		private VByte _numTiles;
-		private VByte _tileIndex;
-		private VByte _stateLength;
-		private VByte _stateLoop;
-		private VByte _stateNext;
-		private Dictionary<string, Label> _frames;
-		private IIterator _iterator;
-		private Models.Animation animConfig;
-		private Label _tileOffsetLabel;
-		private LabelList _stateLabelList;
-		private LabelList _frameLabelList;
+namespace NESSharp.Lib.Animation;
 
-		[Dependencies]
-		private void Dependencies() {
-			_ptr			= Ptr	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_ptr)}");
-			_animData		= Struct.New<AnimationData>(Zp,		$"{nameof(AnimUtil)}{nameof(_animData)}");
-			_numTiles		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_numTiles)}");
-			_tileIndex		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_tileIndex)}");
-			_stateLength	= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_stateLength)}");
-			_stateLoop		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_stateLoop)}");
-			_stateNext		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_stateNext)}");
-			_frames = new Dictionary<string, Label>();
+public class AnimUtil : Module {
+	private Ptr _ptr;
+	private AnimationData _animData;
+	private VByte _numTiles;
+	private VByte _tileIndex;
+	private VByte _stateLength;
+	private VByte _stateLoop;
+	private VByte _stateNext;
+	private Dictionary<string, Label> _frames;
+	private IIterator _iterator;
+	private Models.Animation animConfig;
+	private Label _tileOffsetLabel;
+	private LabelList _stateLabelList;
+	private LabelList _frameLabelList;
+
+	[Dependencies]
+	private void Dependencies() {
+		_ptr			= Ptr	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_ptr)}");
+		_animData		= Struct.New<AnimationData>(Zp,		$"{nameof(AnimUtil)}{nameof(_animData)}");
+		_numTiles		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_numTiles)}");
+		_tileIndex		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_tileIndex)}");
+		_stateLength	= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_stateLength)}");
+		_stateLoop		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_stateLoop)}");
+		_stateNext		= VByte	.New(Zp,					$"{nameof(AnimUtil)}{nameof(_stateNext)}");
+		_frames = new Dictionary<string, Label>();
+	}
+	
+	public void LoadConfig(string file) {
+		if (!Json.TryLoadFile(file, out animConfig)) throw new Exception("Anim config file is bad");
+		//Console.WriteLine(animConfig);
+	}
+	public AnimUtil Initialize(IIterator iterator) {
+		_iterator = iterator;
+		return this;
+	}
+	public void StartFrame() {
+		NES.PPU.OAM.HideAll();
+		_iterator.Reset();
+	}
+	public void EndFrame() {}
+
+	[Subroutine]
+	private void DrawFrame() {
+		Y.Set(0);
+		_numTiles.Set(A.Set(_ptr[Y]));
+		_tileIndex.Set(0);
+		Y.Inc();
+		//X.Set(0);
+		Loop.While_PreCondition_NoInc(() => _tileIndex.NotEquals(_numTiles), tileLoop => {
+			If.True(_iterator.Invalid, tileLoop.Break);
+
+			X.Set(_iterator.Value());
+			NES.PPU.OAM.Object[X].Y.Set(A.Set(_ptr[Y]).Add(_animData.Y));
+			Y.Inc();
+			NES.PPU.OAM.Object[X].Tile.Set(A.Set(_ptr[Y]).Add(_tileOffsetLabel));
+			Y.Inc();
+			//OAM.Object[X].Attr.Set(_ptr[Y]);
+			NES.PPU.OAM.Object[X].Attr.Set(A.Set(_ptr[Y]).Or(_animData.Attr));
+			Y.Inc();
+			//TODO: handle palette change here
+			//A.Set(_ptr[Y]); //compressed array of 4 palette indexes
+
+			NES.PPU.OAM.Object[X].Attr.Set(z => z.Or(_animData.Palette));
+			Y.Inc();
+			//OAM.Object[X].X.Set(A.Set(_ptr[Y]).Add(_animData.X));										//original
+			If.Block(c => c
+				.True(() => _animData.Attr.And(0b01000000).NotEquals(0), () => {
+					//OAM.Object[X].X.Set(A.Set(_ptr[Y]).Subtract(_animData.X));
+					NES.PPU.OAM.Object[X].X.Set(Common.Math.Negate(A.Set(_ptr[Y])).Add(_animData.X).Subtract(8));	//attempt 1
+				})
+				.Else(() => {
+					NES.PPU.OAM.Object[X].X.Set(A.Set(_ptr[Y]).Add(_animData.X));
+				})
+			);
+					
+			//OAM.Object[X].X.Set(A.Set(255).Subtract(_ptr[Y]).And(0b01000000)).Add(_animData.X));
+			Y.Inc();
+			_iterator.Next();
+			_tileIndex.Inc();
+		});
+	}
+
+	public void DrawSingleFrame(AnimationData animData) {
+		//_animData.State.Set(animData.State);
+		//_animData.Counter.Set(animData.Counter);
+		_animData.X.Set(animData.X);
+		_animData.Y.Set(animData.Y);
+		_animData.Attr.Set(animData.Attr);
+		_animData.Palette.Set(animData.Palette);
+		_ptr.PointTo(_frameLabelList[X.Set(A.Set(animData.State))]);
+		AL.GoSub(DrawFrame);
+	}
+	public void DrawSingleFrame(Func<IOperand> x, Func<IOperand> y, Func<IOperand> palette, Func<IOperand> attr, Func<IOperand> state) {
+		//_animData.State.Set(animData.State);
+		//_animData.Counter.Set(animData.Counter);
+		_animData.X.Set(x());
+		_animData.Y.Set(y());
+		_animData.Attr.Set(attr());
+		_animData.Palette.Set(palette());
+		_ptr.PointTo(_frameLabelList[X.Set(A.Set(state()))]);
+		AL.GoSub(DrawFrame);
+	}
+
+	public void DrawSingleObject(IndexingRegister reg, U8 tile, IOperand x, IOperand y, Func<RegisterA> attr) {
+		If.True(_iterator.Valid(), () => {
+			reg.Set(_iterator.Value());
+			NES.PPU.OAM.Object[reg].Y.Set(y);
+			NES.PPU.OAM.Object[reg].Tile.Set(tile);
+			NES.PPU.OAM.Object[reg].Attr.Set(attr());
+			NES.PPU.OAM.Object[reg].X.Set(x);
+			_iterator.Next();
+		});
+	}
+	public void DrawSingleObject(IndexingRegister reg, U8 tile, Func<RegisterA> x, Func<RegisterA> y, Func<RegisterA> attr) {
+		If.True(_iterator.Valid(), () => {
+			reg.Set(_iterator.Value());
+			NES.PPU.OAM.Object[reg].Y.Set(y());
+			NES.PPU.OAM.Object[reg].Tile.Set(tile);
+			NES.PPU.OAM.Object[reg].Attr.Set(attr());
+			NES.PPU.OAM.Object[reg].X.Set(x());
+			_iterator.Next();
+		});
+	}
+
+	public void Update(AnimationData animData) {
+		_animData.State.Set(animData.State);
+		_animData.Counter.Set(animData.Counter);
+		_animData.X.Set(animData.X);
+		_animData.Y.Set(animData.Y);
+		_animData.Attr.Set(animData.Attr);
+		_animData.Palette.Set(animData.Palette);
+		if (animData.State.Index == null) {
+			AL.GoSub(Update);
+		} else {
+			Stack.Preserve(animData.State.Index, () => {
+				AL.GoSub(Update);
+			});
+		}
+		animData.State.Set(_animData.State);
+		animData.Counter.Set(_animData.Counter);
+	}
+	[Subroutine]
+	private void Update() {
+		//NES.PPU.Mask.Set(NES.PPU.LazyMask.Set(z => z.Or(0b10000000)));
+		_ptr.PointTo(_stateLabelList[X.Set(_animData.State)]);
+		_stateLength.Set(_ptr[Y.Set(0)]);
+		Y.Inc();
+		_stateLoop.Set(_ptr[Y]);
+		Y.Inc();
+		_stateNext.Set(_ptr[Y]);
+		Y.Inc(); //now on last frame start
+
+		Loop.Infinite(loop => {
+			If.Block(c => c
+				.True(() => A.Set(_ptr[Y]).LessThanOrEqualTo(_animData.Counter), () => {
+					Y.Inc(); //now on frame ID
+					_ptr.PointTo(_frameLabelList[X.Set(A.Set(_ptr[Y]))]);
+					AL.GoSub(DrawFrame); //Y is no longer needed after this because of the break
+					_animData.Counter.Inc();
+					loop.Break();
+				})
+				.Else(() => Y.Inc()) //now on frame ID
+			);
+			Y.Inc(); //now on frame start
+		});
+		If.True(() => _animData.Counter.Equals(_stateLength), () => {	//is counter maxed out?
+			_animData.Counter.Set(0);								//	reset it
+			If.True(() => _stateLoop.Equals(0), () => {					//if this state doesn't loop,
+				_animData.State.Set(_stateNext);					//	move to the specified next state
+			});
+		});
+		//NES.PPU.Mask.Set(NES.PPU.LazyMask.Set(z => z.And(0b01111111)));
+	}
+	
+	[DataSection]
+	private void AnimData() {
+		//General data
+		_tileOffsetLabel = AL.Labels.New().Write();
+		AL.Raw(animConfig.Offset);
+
+		//State data
+		var stateLabels = new List<Label>();
+		foreach (var state in animConfig.States) {
+			AL.Comment($"Animation state definition: {state.Name}");
+			var lbl = AL.Labels.New().Write();
+			stateLabels.Add(lbl);
+			AL.Raw((byte)state.Length);
+			AL.Raw(state.Loop ? (byte)1 : (byte)0);
+			AL.Raw((byte)(state.NextState ?? 0));
+			foreach (var frame in state.Frames.OrderByDescending(x => (int)x.Start)) {
+				AL.Raw(frame.Start, frame.Id);
+			}
 		}
 		
-		public void LoadConfig(string file) {
-			if (!Json.TryLoadFile(file, out animConfig)) throw new Exception("Anim config file is bad");
-			//Console.WriteLine(animConfig);
-		}
-		public AnimUtil Initialize(IIterator iterator) {
-			_iterator = iterator;
-			return this;
-		}
-		public void StartFrame() {
-			NES.PPU.OAM.HideAll();
-			_iterator.Reset();
-		}
-		public void EndFrame() {}
+		AL.Comment($"Animation state labels");
+		_stateLabelList = new LabelList(stateLabels.ToArray());
+		_stateLabelList.WriteList();
 
-		[Subroutine]
-		private void DrawFrame() {
-			Y.Set(0);
-			_numTiles.Set(A.Set(_ptr[Y]));
-			_tileIndex.Set(0);
-			Y.Inc();
-			//X.Set(0);
-			Loop.While_PreCondition_NoInc(() => _tileIndex.NotEquals(_numTiles), tileLoop => {
-				If.True(_iterator.Invalid, tileLoop.Break);
-
-				X.Set(_iterator.Value());
-				NES.PPU.OAM.Object[X].Y.Set(A.Set(_ptr[Y]).Add(_animData.Y));
-				Y.Inc();
-				NES.PPU.OAM.Object[X].Tile.Set(A.Set(_ptr[Y]).Add(_tileOffsetLabel));
-				Y.Inc();
-				//OAM.Object[X].Attr.Set(_ptr[Y]);
-				NES.PPU.OAM.Object[X].Attr.Set(A.Set(_ptr[Y]).Or(_animData.Attr));
-				Y.Inc();
-				//TODO: handle palette change here
-				//A.Set(_ptr[Y]); //compressed array of 4 palette indexes
-
-				NES.PPU.OAM.Object[X].Attr.Set(z => z.Or(_animData.Palette));
-				Y.Inc();
-				//OAM.Object[X].X.Set(A.Set(_ptr[Y]).Add(_animData.X));										//original
-				If.Block(c => c
-					.True(() => _animData.Attr.And(0b01000000).NotEquals(0), () => {
-						//OAM.Object[X].X.Set(A.Set(_ptr[Y]).Subtract(_animData.X));
-						NES.PPU.OAM.Object[X].X.Set(Common.Math.Negate(A.Set(_ptr[Y])).Add(_animData.X).Subtract(8));	//attempt 1
-					})
-					.Else(() => {
-						NES.PPU.OAM.Object[X].X.Set(A.Set(_ptr[Y]).Add(_animData.X));
-					})
-				);
-						
-				//OAM.Object[X].X.Set(A.Set(255).Subtract(_ptr[Y]).And(0b01000000)).Add(_animData.X));
-				Y.Inc();
-				_iterator.Next();
-				_tileIndex.Inc();
-			});
+		foreach (var frame in animConfig.Frames) {
+			AL.Comment($"Animation frame definition: {frame.Name}");
+			var lbl = AL.Labels.New().Write();
+			_frames.Add(frame.Name, lbl);
+			frame.Write();
 		}
-
-		public void DrawSingleFrame(AnimationData animData) {
-			//_animData.State.Set(animData.State);
-			//_animData.Counter.Set(animData.Counter);
-			_animData.X.Set(animData.X);
-			_animData.Y.Set(animData.Y);
-			_animData.Attr.Set(animData.Attr);
-			_animData.Palette.Set(animData.Palette);
-			_ptr.PointTo(_frameLabelList[X.Set(A.Set(animData.State))]);
-			GoSub(DrawFrame);
-		}
-		public void DrawSingleFrame(Func<IOperand> x, Func<IOperand> y, Func<IOperand> palette, Func<IOperand> attr, Func<IOperand> state) {
-			//_animData.State.Set(animData.State);
-			//_animData.Counter.Set(animData.Counter);
-			_animData.X.Set(x());
-			_animData.Y.Set(y());
-			_animData.Attr.Set(attr());
-			_animData.Palette.Set(palette());
-			_ptr.PointTo(_frameLabelList[X.Set(A.Set(state()))]);
-			GoSub(DrawFrame);
-		}
-
-		public void DrawSingleObject(IndexingRegister reg, U8 tile, IOperand x, IOperand y, Func<RegisterA> attr) {
-			If.True(_iterator.Valid(), () => {
-				reg.Set(_iterator.Value());
-				NES.PPU.OAM.Object[reg].Y.Set(y);
-				NES.PPU.OAM.Object[reg].Tile.Set(tile);
-				NES.PPU.OAM.Object[reg].Attr.Set(attr());
-				NES.PPU.OAM.Object[reg].X.Set(x);
-				_iterator.Next();
-			});
-		}
-		public void DrawSingleObject(IndexingRegister reg, U8 tile, Func<RegisterA> x, Func<RegisterA> y, Func<RegisterA> attr) {
-			If.True(_iterator.Valid(), () => {
-				reg.Set(_iterator.Value());
-				NES.PPU.OAM.Object[reg].Y.Set(y());
-				NES.PPU.OAM.Object[reg].Tile.Set(tile);
-				NES.PPU.OAM.Object[reg].Attr.Set(attr());
-				NES.PPU.OAM.Object[reg].X.Set(x());
-				_iterator.Next();
-			});
-		}
-
-		public void Update(AnimationData animData) {
-			_animData.State.Set(animData.State);
-			_animData.Counter.Set(animData.Counter);
-			_animData.X.Set(animData.X);
-			_animData.Y.Set(animData.Y);
-			_animData.Attr.Set(animData.Attr);
-			_animData.Palette.Set(animData.Palette);
-			if (animData.State.Index == null) {
-				GoSub(Update);
-			} else {
-				Stack.Preserve(animData.State.Index, () => {
-					GoSub(Update);
-				});
-			}
-			animData.State.Set(_animData.State);
-			animData.Counter.Set(_animData.Counter);
-		}
-		[Subroutine]
-		private void Update() {
-			//NES.PPU.Mask.Set(NES.PPU.LazyMask.Set(z => z.Or(0b10000000)));
-			_ptr.PointTo(_stateLabelList[X.Set(_animData.State)]);
-			_stateLength.Set(_ptr[Y.Set(0)]);
-			Y.Inc();
-			_stateLoop.Set(_ptr[Y]);
-			Y.Inc();
-			_stateNext.Set(_ptr[Y]);
-			Y.Inc(); //now on last frame start
-
-			Loop.Infinite(loop => {
-				If.Block(c => c
-					.True(() => A.Set(_ptr[Y]).LessThanOrEqualTo(_animData.Counter), () => {
-						Y.Inc(); //now on frame ID
-						_ptr.PointTo(_frameLabelList[X.Set(A.Set(_ptr[Y]))]);
-						GoSub(DrawFrame); //Y is no longer needed after this because of the break
-						_animData.Counter.Inc();
-						loop.Break();
-					})
-					.Else(() => Y.Inc()) //now on frame ID
-				);
-				Y.Inc(); //now on frame start
-			});
-			If.True(() => _animData.Counter.Equals(_stateLength), () => {	//is counter maxed out?
-				_animData.Counter.Set(0);								//	reset it
-				If.True(() => _stateLoop.Equals(0), () => {					//if this state doesn't loop,
-					_animData.State.Set(_stateNext);					//	move to the specified next state
-				});
-			});
-			//NES.PPU.Mask.Set(NES.PPU.LazyMask.Set(z => z.And(0b01111111)));
-		}
-		
-		[DataSection]
-		private void AnimData() {
-			//General data
-			_tileOffsetLabel = Labels.New().Write();
-			Raw(animConfig.Offset);
-
-			//State data
-			var stateLabels = new List<Label>();
-			foreach (var state in animConfig.States) {
-				Comment($"Animation state definition: {state.Name}");
-				var lbl = Labels.New().Write();
-				stateLabels.Add(lbl);
-				Raw((byte)state.Length);
-				Raw(state.Loop ? (byte)1 : (byte)0);
-				Raw((byte)(state.NextState ?? 0));
-				foreach (var frame in state.Frames.OrderByDescending(x => (int)x.Start)) {
-					Raw(frame.Start, frame.Id);
-				}
-			}
-			
-			Comment($"Animation state labels");
-			_stateLabelList = new LabelList(stateLabels.ToArray());
-			_stateLabelList.WriteList();
-
-			foreach (var frame in animConfig.Frames) {
-				Comment($"Animation frame definition: {frame.Name}");
-				var lbl = Labels.New().Write();
-				_frames.Add(frame.Name, lbl);
-				frame.Write();
-			}
-			Comment($"Animation frame labels");
-			_frameLabelList = new LabelList(_frames.Values.ToArray());
-			_frameLabelList.WriteList();
-		}
+		AL.Comment($"Animation frame labels");
+		_frameLabelList = new LabelList(_frames.Values.ToArray());
+		_frameLabelList.WriteList();
 	}
 }
--- Models/Frame.cs
+++ Models/Frame.cs
@@ -1,15 +1,14 @@
 ﻿using NESSharp.Core;
 using System.Collections.Generic;
-using static NESSharp.Core.AL;
 
-namespace NESSharp.Lib.Animation.Models {
-	public class Frame : IWritable {
-		public string Name { get; set; }
-		public List<SObject> Tiles { get; set; }
-		public void Write() {
-			Raw((U8)Tiles.Count);
-			foreach(var sobj in Tiles)
-				sobj.Write();
-		}
+namespace NESSharp.Lib.Animation.Models;
+
+public class Frame : IWritable {
+	public string Name { get; set; }
+	public List<SObject> Tiles { get; set; }
+	public void Write() {
+		AL.Raw((U8)Tiles.Count);
+		foreach(var sobj in Tiles)
+			sobj.Write();
 	}
 }
